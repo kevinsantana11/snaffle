fd4e0a459263dfdc3909a785edd446e4b4554a8a
Kevin Santana <Kevin@Kevins-Mac-mini.local>
2019-01-18 22:53:04 -0500

    Initial XCode Project

---
+++ snaffle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snaffle
+//
+//  Created by Kevin Santana on 1/18/19.
+//  Copyright © 2019 Kevin Santana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
